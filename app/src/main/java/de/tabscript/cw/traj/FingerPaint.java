0625ef373d51909741d84ab3db77e7e1e5a2e5e4
Christian Wieprecht <christian.wieprecht@udo.edu>
2015-03-07 12:29:04 +0100

    Added option to use wordlist to create annotations.

--- app/src/main/java/de/tabscript/cw/traj/FingerPaint.java
+++ app/src/main/java/de/tabscript/cw/traj/FingerPaint.java
@@ -19,9 +19,11 @@ package de.tabscript.cw.traj;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.*;
 import android.os.Bundle;
 import android.os.Environment;
@@ -29,6 +31,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -36,25 +39,41 @@ public class FingerPaint extends Activity {
 
     private WritingView mWritingView;
     private TextView mTouchToleranceLabel;
+    private TextView mCurrentWordView;
+    private ArrayList<String> mWordList;
+    private String mListName;
+    private int mCurrentWordIndex;
+    private boolean mUsingWords;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.finger_paint);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.finger_paint);
         mWritingView = (WritingView) findViewById(R.id.writing_surface_view);
-		mTouchToleranceLabel = (TextView) findViewById(R.id.coords);
+        mTouchToleranceLabel = (TextView) findViewById(R.id.coords);
         mTouchToleranceLabel.setText("" + mWritingView.getmTouchTolerance());
-	}
+        Intent intent = getIntent();
+        mWordList = intent.getStringArrayListExtra("words");
+        mListName = intent.getStringExtra("listname");
+        if (mWordList != null) {
+            ((Button) findViewById(R.id.save_button)).setText("Save&Next");
+            mUsingWords = true;
+            mCurrentWordIndex = 0;
+            mCurrentWordView = (TextView) findViewById(R.id.current_word_textview);
+            mCurrentWordView.setVisibility(View.VISIBLE);
+            mCurrentWordView.setText(mWordList.get(mCurrentWordIndex));
+        }
+    }
 
-	public void onRaiseToleranceClicked(View v) {
-		mWritingView.raiseTouchTolerance();
+    public void onRaiseToleranceClicked(View v) {
+        mWritingView.raiseTouchTolerance();
         updateTouchToleranceLabel();
-	}
+    }
 
-	public void onLowerToleranceClicked(View v) {
-		mWritingView.lowerTouchTolerance();
+    public void onLowerToleranceClicked(View v) {
+        mWritingView.lowerTouchTolerance();
         updateTouchToleranceLabel();
-	}
+    }
 
     private void updateTouchToleranceLabel() {
         if (mTouchToleranceLabel != null) {
@@ -68,41 +87,58 @@ public class FingerPaint extends Activity {
 
     public void onSaveButtonClicked(View v) {
         savePointListToFile(mWritingView.getPointList());
+        if (mUsingWords) {
+            mCurrentWordView.setText(mWordList.get(++mCurrentWordIndex));
+        }
+        mWritingView.clearCanvas();
     }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		super.onCreateOptionsMenu(menu);
-		return false;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        return false;
+    }
 
-	@Override
-	public boolean onPrepareOptionsMenu(Menu menu) {
-		super.onPrepareOptionsMenu(menu);
-		return true;
-	}
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        return true;
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
 
     private void savePointListToFile(List<Point> pointList) {
         File folder = new File(Environment.getExternalStorageDirectory() + "/TraJData");
         if (!folder.exists()) {
             boolean success = folder.mkdir();
-            if(!success) {
+            if (!success) {
                 Toast.makeText(this, "Creating folder '<externalStorage>/TraJData' failed.", Toast.LENGTH_SHORT).show();
+                return;
             }
         }
+        File docFolder = new File(Environment.getExternalStorageDirectory() + "/TraJData/" + mListName);
+        if (mUsingWords) {
+            if (!docFolder.exists()) {
+                boolean success = docFolder.mkdir();
+                if (!success) {
+                    Toast.makeText(this, "Creating folder '<externalStorage>/TraJData" + mListName + "/' failed!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+            // oh goood... what am I doing...
+            folder = docFolder;
+        }
         FileWriter fw;
-        Long tsLong = System.currentTimeMillis();
-        String ts = tsLong.toString();
-        String filename = "traj_" + ts + ".txt";
+        String filename = buildFilename();
         try {
             fw = new FileWriter(folder.getAbsolutePath() + "/" + filename, false);
-            // fw.write("word\n");
-            for(Point p: pointList) {
+            if (mUsingWords) {
+                fw.write(mWordList.get(mCurrentWordIndex) + "\n");
+            }
+            for (Point p : pointList) {
                 fw.write("" + p.x + " " + p.y + "\n");
             }
             fw.flush();
@@ -112,5 +148,15 @@ public class FingerPaint extends Activity {
             e.printStackTrace();
             Log.e("Error", e.getMessage());
         }
+    }
+
+    private String buildFilename() {
+        Long tsLong = System.currentTimeMillis();
+        String ts = tsLong.toString();
+        String filename = "" + ts;
+        if (mUsingWords) {
+            filename += "_" + mWordList.get(mCurrentWordIndex);
+        }
+        return filename + ".txt";
     }
 }
--- app/src/main/java/de/tabscript/cw/traj/MainActivity.java
+++ app/src/main/java/de/tabscript/cw/traj/MainActivity.java
@@ -1,11 +1,21 @@
 package de.tabscript.cw.traj;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -26,6 +36,36 @@ public class MainActivity extends Activity {
     }
 
     public void startWithSource(View view) {
-        Toast.makeText(this, "Not yet implemented", Toast.LENGTH_SHORT).show();
+        File mPath = new File(Environment.getExternalStorageDirectory() + "/TrajData/");
+        FileDialog fileDialog = new FileDialog(this, mPath);
+        final Context context = this.getApplicationContext();
+        fileDialog.addFileListener(new FileDialog.FileSelectedListener() {
+            public void fileSelected(File file) {
+                Log.d(getClass().getName(), "selected file " + file.toString());
+                ArrayList<String> wordList = new ArrayList<String>();
+                try {
+                    BufferedReader br = new BufferedReader(new FileReader(file));
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        wordList.add(line.trim());
+                    }
+                    br.close();
+                }
+                catch (IOException e) {
+                    Toast.makeText(context, "Error reading file!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String listName = file.getName().replaceFirst("[.][^.]+$", "");
+                intentWithSource(wordList, listName);
+            }
+        });
+        fileDialog.showDialog();
+    }
+
+    private void intentWithSource(ArrayList<String> wordList, String listName) {
+        Intent i = new Intent(this, FingerPaint.class);
+        i.putStringArrayListExtra("words", wordList);
+        i.putExtra("listname", listName);
+        startActivity(i);
     }
 }
